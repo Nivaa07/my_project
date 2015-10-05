b7d808d9968b55cab2f1e49c270afafee7dbeb97
Nivaa07 <sasibaskaran@et.esiea.fr>
2015-10-05 16:03:22 +0200

    Programmation Web
    Cours 1

---
+++ README.md
@@ -0,0 +1,4 @@
+my_project
+==========
+
+A Symfony project created on October 5, 2015, 6:58 am.
---
+++ src/OC/ArticleBundle/Controller/AdvertController.php
@@ -0,0 +1,17 @@
+<?php
+    
+    namespace OC\ArticleBundle\Controller;
+    
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\HttpFoundation\Response;
+   
+    class AdvertController extends Controller
+    {
+        public function indexAction()
+        {
+        $content = $this
+            ->get('templating')
+            ->render('OCArticleBundle:Advert:index.html.twig');
+    return new Response($content);
+        }
+    }
